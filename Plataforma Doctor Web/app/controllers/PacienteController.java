b22133c5ec45a540038d02a3f75ada1623d76e8d
Nicolas Chaves de Plaza <chadepl@gmail.com>
2015-02-17 11:03:52 -0500

    PacienteController modificado

--- Plataforma Doctor Web/app/controllers/PacienteController.java
+++ Plataforma Doctor Web/app/controllers/PacienteController.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import models.Doctor;
+import models.Paciente;
 import play.data.Form;
 import play.mvc.*;
 import views.html.*;
@@ -14,6 +14,42 @@ public class PacienteController extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-    
+
+    public static Result actualizarPaciente(String id){
+        ArrayList<Paciente> pacientes=inicializarArreglo();
+        Paciente paciente =Form.form(Paciente.class).bindFromRequest().get();
+        paciente.setIdentificacion(id);
+        //Busqueda por JPA
+        //Persistir por JPA
+        if(reemplazarPaciente(pacientes,paciente)){
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode node = mapper.convertValue(paciente, JsonNode.class);
+            return ok(node);
+        }
+        else{
+            return status(1, "Doctor no existente");
+        }
+    }
+
+    private static ArrayList<Paciente> inicializarArreglo(){
+        ArrayList<Paciente> pacientes = new ArrayList<Paciente>();
+        for (int i = 0; i < 10; i++) {
+            Paciente nuevo = new Paciente();
+            nuevo.setIdentificacion(i+"");
+            pacientes.add(nuevo);
+        }
+        return pacientes;
+    }
+
+    private static boolean reemplazarPaciente(ArrayList<Paciente> pacientes,Paciente paciente){
+        boolean termino=false;
+        for (int i = 0; i < pacientes.size() && !termino; i++) {
+            if(pacientes.get(i).getIdentificacion().equals(paciente.getIdentificacion())){
+                pacientes.add(i, paciente);
+                termino=true;
+            }
+        }
+        return termino;
+    }
   
 }
