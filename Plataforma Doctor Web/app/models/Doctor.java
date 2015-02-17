bb14170205d41cb3e4c9c34d07dee9a7bf5c647e
sfrsebastian <sfrsebastian@gmail.com>
2015-02-16 21:37:54 -0500

    Servicio de actualizar doctor y Modelo y Controlador Doctor

---
+++ Plataforma Doctor Web/app/models/Doctor.java
@@ -0,0 +1,145 @@
+package models;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+import play.libs.F.Option;
+import play.mvc.QueryStringBindable;
+
+public class Doctor implements QueryStringBindable<Doctor>{
+	private String nombre;
+	private String apellido;
+	private String identificacion;
+	private boolean autorizado;
+	private String password;
+	private Date fechaVinculacion;
+	private Date fechaNacimiento;
+	
+	//Anotaciones JPA
+//	private Doctor colegas;
+//	private Episodio segundasOpiniones;
+//	private Comentario comentarios;
+//	private Paciente pacientes;
+
+	
+//	public Doctor getColegas() {
+//		return colegas;
+//	}
+//
+//	public void setColegas(Doctor colegas) {
+//		this.colegas = colegas;
+//	}
+//
+//	public Episodio getSegundasOpiniones() {
+//		return segundasOpiniones;
+//	}
+//
+//	public void setSegundasOpiniones(Episodio segundasOpiniones) {
+//		this.segundasOpiniones = segundasOpiniones;
+//	}
+//
+//	public Comentario getComentarios() {
+//		return comentarios;
+//	}
+//
+//	public void setComentarios(Comentario comentarios) {
+//		this.comentarios = comentarios;
+//	}
+//
+//	public Paciente getPacientes() {
+//		return pacientes;
+//	}
+//
+//	public void setPacientes(Paciente pacientes) {
+//		this.pacientes = pacientes;
+//	}
+
+	public String getIdentificacion() {
+		return identificacion;
+	}
+	
+	public void setIdentificacion(String identificacion) {
+		this.identificacion = identificacion;
+	}
+	
+	public boolean isAutorizado() {
+		return autorizado;
+	}
+	
+	public void setAutorizado(boolean autorizado) {
+		this.autorizado = autorizado;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Date getFechaVinculacion() {
+		return fechaVinculacion;
+	}
+	
+	public void setFechaVinculacion(Date fechaVinculacion) {
+		this.fechaVinculacion = fechaVinculacion;
+	}
+	
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	@Override
+	public Option<Doctor> bind(String key, Map<String, String[]> values) {
+		try{
+			this.nombre = values.containsKey("nombre")?values.get("nombre")[0]:"";
+			this.apellido = values.containsKey("apellido")?values.get("apellido")[0]:"";
+			this.identificacion = values.containsKey("identificacion")?values.get("identificacion")[0]:"";
+			this.autorizado=false;
+			this.password = values.containsKey("password")?values.get("password")[0]:"";
+			this.fechaVinculacion = Calendar.getInstance().getTime();
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
+			this.fechaNacimiento=sdf.parse(values.get("fechaNacimiento")[0]);
+			return Option.Some(this);
+		}
+		catch(Exception e){
+			return Option.None();
+		}
+		
+	}
+	
+	@Override
+	public String javascriptUnbind() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String unbind(String arg0) {
+		return null;
+	}
+}
