d012910583a07ef7153765c3cebd5659bc62dffa
sfrsebastian <sfrsebastian@gmail.com>
2015-02-17 18:02:39 -0500

    Anotaciones JPA Episodio.

--- Plataforma Doctor Web/app/models/Episodio.java
+++ Plataforma Doctor Web/app/models/Episodio.java
@@ -1,101 +1,148 @@
 package models;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 import java.util.Map;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import play.libs.F.Option;
 import play.mvc.QueryStringBindable;
 
+@Entity
 public class Episodio implements QueryStringBindable<Episodio>{
 
-	private String nivelDolor;
-	private String fecha;
-	private String localizacion;
-	
-	private String comentarios;
-	private String[] medicamentos;
-	private String[] patronesSueno;
-	private String[] causas;
-	
-	@Override
-	public Option<Episodio> bind(String key, Map<String, String[]> values) {
-		try{
-			this.nivelDolor = values.containsKey("intensidad")?values.get("intensidad")[0]:"";
-			this.localizacion = values.containsKey("descripcion")?values.get("descripcion")[0]:"";
-			this.comentarios = values.containsKey("comentarios")?values.get("comentarios")[0]:"";
-			
-			//Falta inicializar los arreglos!
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
-			this.fecha =  sdf.format(Calendar.getInstance().getTime());
-			this.fecha=values.get("fecha")[0];
-			return Option.Some(this);
-		}
-		catch(Exception e){
-			return Option.None();
-		}
-		
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private int nivelDolor;
+
+	private Date fecha;
+
+	private Causa localizacion;
+
+	@OneToMany
+	private List<Comentario> comentarios;
+
+	@ManyToMany
+	private List<Medicamento> medicamentos;
+
+	@ManyToMany
+	private List<Intervalo> patronesSueno;
+
+	@ManyToMany
+	private List<Causa> causas;
+
+	public Long getId() {
+		return id;
 	}
 
-	public Date getFecha() throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
-		return fecha!=null?sdf.parse(fecha):null;
-	}
-	
-	public void setFecha(String fecha) {
-		this.fecha = fecha;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
-	public String getNivelDolor() {
+	public int getNivelDolor() {
 		return nivelDolor;
 	}
 
-	public void setNivelDolor(String intensidad) {
-		this.nivelDolor = intensidad;
+	public void setNivelDolor(int nivelDolor) {
+		this.nivelDolor = nivelDolor;
 	}
 
-	public String getLocalizacion() {
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Causa getLocalizacion() {
 		return localizacion;
 	}
 
-	public void setLocalizacion(String descripcion) {
-		this.localizacion = descripcion;
+	public void setLocalizacion(Causa localizacion) {
+		this.localizacion = localizacion;
 	}
-	
-	public String getComentarios() {
+
+	public List<Comentario> getComentarios() {
 		return comentarios;
 	}
 
-	public void setComentarios(String comentarios) {
+	public void setComentarios(List<Comentario> comentarios) {
 		this.comentarios = comentarios;
 	}
 
-	public String[] getMedicamentos() {
+	public void addComentario(Comentario comentario){
+		if(this.comentarios==null){
+			this.comentarios=new ArrayList<Comentario>();
+		}
+		this.comentarios.add(comentario);
+	}
+
+	public List<Medicamento> getMedicamentos() {
 		return medicamentos;
 	}
 
-	public void setMedicamentos(String[] medicamentos) {
+	public void setMedicamentos(List<Medicamento> medicamentos) {
 		this.medicamentos = medicamentos;
 	}
 
-	public String[] getPatronesSueno() {
+	public List<Intervalo> getPatronesSueno() {
 		return patronesSueno;
 	}
 
-	public void setPatronesSueno(String[] patronesSueno) {
+	public void setPatronesSueno(List<Intervalo> patronesSueno) {
 		this.patronesSueno = patronesSueno;
 	}
 
-	public String[] getCausas() {
+	public void addPatronDeSueno(Intervalo patron){
+		if(this.patronesSueno==null){
+			this.patronesSueno=new ArrayList<Intervalo>();
+		}
+		this.patronesSueno.add(patron);
+	}
+
+	public List<Causa> getCausas() {
 		return causas;
 	}
 
-	public void setCausas(String[] causas) {
+	public void setCausas(List<Causa> causas) {
 		this.causas = causas;
+	}
+
+	public void addCausa(Causa causa){
+		if(this.causas==null){
+			this.causas=new ArrayList<Causa>();
+		}
+		this.causas.add(causa);
+	}
+
+	@Override
+	public Option<Episodio> bind(String key, Map<String, String[]> values) {
+//		try{
+//			this.nivelDolor = values.containsKey("intensidad")?values.get("intensidad")[0]:"";
+//			this.localizacion = values.containsKey("descripcion")?values.get("descripcion")[0]:"";
+//			this.comentarios = values.containsKey("comentarios")?values.get("comentarios")[0]:"";
+//
+//			//Falta inicializar los arreglos!
+//
+//			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
+//			this.fecha =  sdf.format(Calendar.getInstance().getTime());
+//			this.fecha=values.get("fecha")[0];
+//			return Option.Some(this);
+//		}
+//		catch(Exception e){
+//			return Option.None();
+//		}
+		return Option.None();
 	}
 
 	@Override
