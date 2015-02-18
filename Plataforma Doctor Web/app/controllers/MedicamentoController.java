377a9f498ad964bcbde2575b396eed87469cae53
Mario Hernandez <med.hernandez10@gmail.com>
2015-02-18 08:39:45 -0500

    Terminado servicio 5

--- Plataforma Doctor Web/app/controllers/MedicamentoController.java
+++ Plataforma Doctor Web/app/controllers/MedicamentoController.java
@@ -1,5 +1,12 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import models.Medicamento;
+import play.data.Form;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -9,4 +16,22 @@ public class MedicamentoController extends Controller {
 	public static Result index() {
 		return ok(index.render("Your new application is ready."));
 	}
+	
+    //Mario
+    @Transactional
+	public static Result crearMedicamento(){
+    	Medicamento nuevo = Form.form(Medicamento.class).bindFromRequest().get();
+		Medicamento actual = JPA.em().find(Medicamento.class, nuevo.getId());
+		if(actual==null){
+			
+			JPA.em().persist(nuevo);			
+			ObjectMapper mapper = new ObjectMapper(); 
+			JsonNode node = mapper.convertValue(nuevo, JsonNode.class);
+			return ok(node);
+		}	
+		else{
+			return status(1,"El medicamento ya existe");
+		}
+	}
+
 }
