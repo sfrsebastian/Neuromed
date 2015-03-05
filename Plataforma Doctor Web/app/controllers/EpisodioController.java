31146ec4b2f28426bdd216664c1c09688bfd2ac5
sfrsebastian <sfrsebastian@gmail.com>
2015-03-04 20:15:47 -0500

    crear comentario corregido

--- Plataforma Doctor Web/app/controllers/EpisodioController.java
+++ Plataforma Doctor Web/app/controllers/EpisodioController.java
@@ -28,23 +28,28 @@ public class EpisodioController extends Controller {
 		}
 	}
 
-	
-	
 	@Transactional
 	public static Result crearComentario(Long id){
+		JsonNode json = request().body().asJson();
 		Episodio actual = JPA.em().find(Episodio.class, id);
+		Long idDoctor = json.path("idDoctor").asLong();
+		Doctor doctor = JPA.em().find(Doctor.class, idDoctor);
 		if(actual!=null){
 			Comentario nuevo = Form.form(Comentario.class).bindFromRequest().get();
 			JPA.em().persist(nuevo);
 			actual.addComentario(nuevo);
+			doctor.addComentario(nuevo);
 			List<Comentario> comentarios = actual.getComentarios();
 			ObjectMapper mapper = new ObjectMapper(); 
 			JsonNode node = mapper.convertValue(comentarios, JsonNode.class);
 			JPA.em().merge(actual);
+			JPA.em().merge(doctor);
 			return ok(node);
 		}	
 		else{
 			return ok("El episodio con identificacion: " + id+ " no existe en el sistema, o el doctor dado no existe.");
 		}
 	}
+	
+	
 }
