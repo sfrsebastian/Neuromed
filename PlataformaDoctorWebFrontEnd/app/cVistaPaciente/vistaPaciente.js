83e545fd4bd183e3236acd89d71ead2cb3e38a27
Nicolas Chaves de Plaza <chadepl@gmail.com>
2015-03-25 16:12:15 -0500

    Agregados date pickers e implementacion del metodo buscar por fecha, falta mostrarlo en la interfaz web

--- PlataformaDoctorWebFrontEnd/app/cVistaPaciente/vistaPaciente.js
+++ PlataformaDoctorWebFrontEnd/app/cVistaPaciente/vistaPaciente.js
@@ -134,25 +134,15 @@ angular.module('mVistaPaciente', ['ngRoute'])
             }
 
             $scope.buscarRangoFecha=function(){
-                window.alert("Funcion en desarrollo");
+                window.alert("Funcion en desarrollo"+$scope.fechaI+" "+$scope.fechaF);
+                $http.get('http://neuromed.herokuapp.com/api/paciente/'+$scope.idPaciente+'/episodio'+$scope.fechaI+'/'+$scope.fechaF).then(function(resp) {
+                    console.log('SuccessFecha', resp);
+                    $scope.episodiosF=resp.data;
+                    // For JSON responses, resp.data contains the result
+                });
+
             };
 
-            $scope.data = {
-                series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
-                data: [{
-                    x: "Laptops",
-                    y: [100, 500, 0],
-                    tooltip: "this is tooltip"
-                }, {
-                    x: "Desktops",
-                    y: [300, 100, 100]
-                }, {
-                    x: "Mobiles",
-                    y: [351]
-                }, {
-                    x: "Tablets",
-                    y: [54, 0, 879]
-                }]
-            };
+
 }]);
 
